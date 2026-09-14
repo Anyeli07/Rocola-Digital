1dcc0f4633ea9b941b55175e66979fa47a098373
Anyeli Hernández <anyelih1307@gmail.com>
2026-09-14 09:13:14 -0600

    Rocola Digital

---
+++ php.php
@@ -0,0 +1,25 @@
+<?php
+
+$canciones = $_POST["canciones"];
+ 
+switch($canciones){
+case 1;
+echo "Cancion 1  <br>";
+break;
+case 2;
+echo "Cancion 2 <br>";
+break;
+case 3;
+echo "Cancion 3 <br>";
+break;
+case 3;
+echo "Cancion 4 <br>";
+break;
+case 3;
+echo "Cancion 5 <br>";
+break;
+
+}
+ echo "La cancion que selecciono es:"  . $canciones;
+
+?>
