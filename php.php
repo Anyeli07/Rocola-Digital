137fb56ccbbcace1e30fc46bf32c462103ef1c1a
Anyeli Hernández <anyelih1307@gmail.com>
2026-09-14 10:59:43 -0600

    condicionales

--- php.php
+++ php.php
@@ -3,6 +3,14 @@
 $canciones = $_POST["canciones"];
 $repeticiones = $_POST["repeticiones"];
 $Crepeticiones = $repeticiones * 5;
+$dinero = $_POST["dinero"];
+$cambio = $dinero - $Crepeticiones;
+
+ echo "<h1> Estado:</h1>";
+if ($Crepeticiones > $dinero){
+echo "El dinero no es suficiente para llevar a cabo la compra";
+} else { echo "Compra realizada exitosamente";
+}
  
 switch($canciones){
 case 1;
@@ -22,10 +30,17 @@ echo "Cancion 5 <br>";
 break;
 
 }
- echo "<h1>La cancion que selecciono es:</h1>"  . $canciones;
+ 
+echo "<h1>La cancion que selecciono es:</h1>"  . $canciones;
 
  echo "<h1> Tus repeticiones solicitadas serian: </h1>" . $repeticiones;
  echo "<h1> Total a pagar:</h1>" ."Q."  .  $Crepeticiones;
+ echo "<br>";
+ echo "<br>";
+ echo "<br>";
+
+ echo "<h1> Dinero ingresado: </h1>" . "Q." . $dinero;
+ echo "<h1> Cambio:</h1>" ."Q."  .  $cambio;
  echo "<br>";
  echo "<br>";
  echo "<br>";
